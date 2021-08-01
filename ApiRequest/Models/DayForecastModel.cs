a847a6005552b3ca27a12f5823bf00b541802dae
oğuzhan önal <oguzhanonal52@hotmail.com>
2021-08-01 18:22:26 +0300

    Add project files.

---
+++ ApiRequest/Models/DayForecastModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiRequest.Models
+{
+    public class DayForecastModel
+    {
+        public string Weather_state_name { get; set; }
+        public string Applicable_date { get; set; }
+        public float Min_temp { get; set; }
+        public float Max_temp { get; set; }
+    }
+
+
+
+}
---
+++ ApiRequest/Models/WeatherForecastModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ApiRequest.Models
+{
+
+
+    public class WeatherForecastModel
+    {
+        public DayForecastModel[] Consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; }
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
+    }
+
+
+
+}
